d62da0cc23a53341c67cb2cd8c39b0e7d250f4fc
Roberto Athos <robertoathos22@gmail.com>
2022-07-10 09:35:06 -0300

    destination section

--- src/Pages/Destination/Destination.js
+++ src/Pages/Destination/Destination.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {BodyContainer, Container} from './styles'
+import Header from '../../Components/Header/Header'
+import * as destinations from '../../data.json'
 
 function Destination() {
+
+  const [planets] = useState(destinations)
+  const [value,setValue] =useState(0)
+
+  const {name, images, description, distance, travel} = planets[value]
+
   return (
-    <div>Destination</div>
+    <BodyContainer>
+      <Header/>
+      <Container>
+        <div className='planet'>
+          <h2><span>01</span> PICK YOUR DESTINATION</h2>
+          <img src={images.png} alt={name} title={name} width={"350px"}/>
+        </div>
+        <div className='planet-text'>
+
+            {planets.map((items, index)=>(
+              <button key={index} onClick={()=>setValue(index)}>
+                {items.name}
+              </button>
+            ))}
+            
+            <div className='main-text'>
+                <h3>{name}</h3>
+                <p>{description}</p>
+            </div>
+            <div className='infos'>
+              <div className='infos-text'>
+                 <ul>
+                  <label>AVB.DISTANCE</label>
+                    <li>{distance}</li>
+                    <label>EST.TRAVEL TIME</label>
+                    <li>{travel}</li>
+                 </ul>
+              </div>
+            </div>
+
+        </div>
+      </Container>
+    </BodyContainer>
   )
 }
 
---
+++ src/Pages/Destination/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+import background2 from '../../assets/destination/background-destination-desktop.jpg'
+
+export const BodyContainer = styled.body`
+    background-image: url(${background2});
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100%;
+
+`
+export const Container = styled.section`
+    width: 100%;
+    max-width: 1385px;
+    margin: auto;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: white;
+    height: 100vh;
+    .planet-text{
+        width: 100%;
+        max-width: 445px;
+        height: 472px;
+        h3{
+            font-size: 6.3rem;
+            font-family: 'Bellefair', serif;
+        }
+        p{
+            color: #D0D6F9;
+            font-family: 'Barlow Condensed', sans-serif;
+            font-size: 1.03rem;
+        }
+    }
+    button{
+        color:white;
+        margin: 20px;
+        
+    }
+
+
+`
